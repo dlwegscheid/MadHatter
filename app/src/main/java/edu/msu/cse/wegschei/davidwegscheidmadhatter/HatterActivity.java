ed11e1982ed08cc73e1d7677b5c44578a87c3023
David Wegscheid <wegschei@msu.edu>
2015-02-27 12:19:24 -0500

    Gets picture from gallery

--- app/src/main/java/edu/msu/cse/wegschei/davidwegscheidmadhatter/HatterActivity.java
+++ app/src/main/java/edu/msu/cse/wegschei/davidwegscheidmadhatter/HatterActivity.java
@@ -1,17 +1,60 @@
 package edu.msu.cse.wegschei.davidwegscheidmadhatter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 
 
 public class HatterActivity extends ActionBarActivity {
+
+    /**
+     * The hatter view object
+     */
+    private HatterView hatterView = null;
+
+    /**
+     * The color select button
+     */
+    private Button colorButton = null;
+
+    /**
+     * The feather checkbox
+     */
+    private CheckBox featherCheck = null;
+
+    /**
+     * The hat choice spinner
+     */
+    private Spinner spinner;
+
+    /**
+     * Request code when selecting a picture
+     */
+    private static final int SELECT_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hatter);
+
+        /*
+         * Get some of the views we'll keep around
+         */
+        hatterView = (HatterView)findViewById(R.id.hatterView);
+        colorButton = (Button)findViewById(R.id.buttonColor);
+        featherCheck = (CheckBox)findViewById(R.id.checkFeather);
+        spinner = (Spinner) findViewById(R.id.spinnerHat);
     }
 
 
@@ -35,5 +78,50 @@ public class HatterActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handle a Picture button press
+     * @param view The view this is called from
+     */
+    public void onPicture(View view) {
+        // Get a picture from the gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
+            // Response from the picture selection activity
+            Uri imageUri = data.getData();
+
+            // We have to query the database to determine the document ID for the image
+            Cursor cursor = getContentResolver().query(imageUri, null, null, null, null);
+            cursor.moveToFirst();
+            String document_id = cursor.getString(0);
+            document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+            cursor.close();
+
+            // Next, we query the content provider to find the path for this
+            // document id.
+            cursor = getContentResolver().query(
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+            cursor.moveToFirst();
+            String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            cursor.close();
+
+            if(path != null) {
+                Log.i("Path", path);
+                //hatterView.setImagePath(path);
+            }
+
+        }
     }
 }
